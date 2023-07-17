cf2ec1c9c0ffc38619d7aac03b5229496c8e0f33
Sunil <sunilcscmail@gmail.com>
2023-07-17 20:34:54 +0530

    Number to excel coulmn name

--- DS_And_Algo/src/maths/NumberToExcelCoulmnName.java
+++ DS_And_Algo/src/maths/NumberToExcelCoulmnName.java
@@ -1,31 +1,21 @@
 package maths;
 
 public class NumberToExcelCoulmnName {
-	
+
 	public static void main(String[] args) {
-		
-		
-StringBuilder ans = new StringBuilder();
-		
 
-int n = 943566;
-while (n > 0)
-{
-    // find the index of the next letter and concatenate the letter
-    // to the solution
+		StringBuilder ans = new StringBuilder();
 
-    // here index 0 corresponds to 'A', and 25 corresponds to 'Z'
-    int index = (n - 1) % 26;
-    ans.append((char)(index + 'A'));
-    n = (n - 1) / 26;
-}	
+		int n = 943566;
+		while (n > 0) {
 		
+			int index = (n - 1) % 26;
+			ans.append((char) (index + 'A'));
+			n = (n - 1) / 26;
+		}
+
 		System.out.println(ans.toString());
-		
-		
-		
-	
-		
+
 	}
 
 }
